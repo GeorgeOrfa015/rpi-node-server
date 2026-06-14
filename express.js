2e204ea170e8249b24c81c16ced360909bdb5647
George Orfanides <99083720+GeorgeOrfa015@users.noreply.github.com>
2026-06-14 21:17:17 +0300

    Commit 9 on main Added CORS header.

--- express.js
+++ express.js
@@ -8,6 +8,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello, World!' });
